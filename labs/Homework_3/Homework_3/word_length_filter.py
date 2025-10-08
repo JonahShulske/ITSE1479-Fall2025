eb402f3aaf1d043f22b1b34fb5bc262eeb14e78a
Jonah Shulske <jonah.shulske@my.tccd.edu>
2025-10-08 10:53:58 -0500

    I guess I gotta manually stage stuff now?

--- labs/Homework_3/Homework_3/word_length_filter.py
+++ labs/Homework_3/Homework_3/word_length_filter.py
@@ -1,2 +1,19 @@
+"""
+Jonah Shulske
+October 8, 2025
+ITSE-1479 Fall 2025
+"""
+
+# Problem 1 - Word Length Filter
 sentence = input("Input a sentence: ")
-print(sentence)
+
+split_sen  = sentence.split()
+print(split_sen)
+
+for word in split_sen:
+    if len(word) < 4:
+        print(f"{word} is less than 4 chars")
+    elif len(word) < 6:
+        print(f"{word} is less than 6 chars but bigger than 4")
+    else:
+        print(f"{word} is bigger than 6")
